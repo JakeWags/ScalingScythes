e5d611fd41f84ec1c4ccb9b797161a4ed70a4a66
JakeWags <jakewagoneredu@gmail.com>
2026-01-24 12:30:24 -0700

    Add project files.

---
+++ ScalingScythes/ScalingScythesModSystem.cs
@@ -0,0 +1,16 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace ScalingScythes
+{
+    public class ScalingScythesModSystem : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+            // Register our custom scythe item class
+            api.RegisterItemClass("ItemScytheTiered", typeof(ItemScytheTiered));
+        }
+    }
+}
